bc8c4d1d1d221fc434063d996765d7128afd92ac
Pedro Henrique <p13dr0h@gmail.com>
2022-11-18 01:20:28 -0300

    fix: test fixed

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,6 +12,6 @@ describe("SignUp Controller", ()=>{
         }
         const httpResponse = sut.handle(httpRequest)
 
-        expect(httpResponse.statusCode).toBe(4100)
+        expect(httpResponse.statusCode).toBe(400)
     })
 })
