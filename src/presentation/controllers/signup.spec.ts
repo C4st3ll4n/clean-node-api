f72dca1e166dbd1b33d651e8c459823ac3c2337b
Pedro Henrique <p13dr0h@gmail.com>
2022-12-07 15:42:45 -0300

    feat: ensure password and confirmation password are equals

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,17 +18,6 @@ const makeEmailValidator = (): EmailValidator => {
     return new EmailValidatorStub()
 }
 
-
-const makeEmailValidatorWithError = (): EmailValidator => {
-    class EmailValidatorStub implements EmailValidator {
-        isValid(email: string): boolean {
-            throw new Error()
-        }
-    }
-
-    return new EmailValidatorStub()
-}
-
 const makeSut = (): SutTypes => {
 
     const emailValidatorStub = makeEmailValidator()
@@ -101,6 +90,7 @@ describe("SignUp Controller", () => {
         expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
     })
 
+    
     test("Should return 400 if invalid email is provided", () => {
         const { sut, emailValidatorStub } = makeSut()
 
@@ -118,6 +108,26 @@ describe("SignUp Controller", () => {
 
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new InvalidParamError("email"))
+
+    })
+
+    test("Should return 400 if invalid confirmation password is provided", () => {
+        const { sut, emailValidatorStub } = makeSut()
+
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "invalid_email",
+                password: "any_password",
+                passwordConfirmation: "invalid_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError("passwordConfirmation"))
 
     })
 
@@ -140,9 +150,10 @@ describe("SignUp Controller", () => {
 
     test("Should return 500 if EmailValidator throws", () => {
 
-        const emailValidatorStub = makeEmailValidatorWithError()
-        const sut = new SignUpController(emailValidatorStub)
-
+        const {sut, emailValidatorStub} = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(()=>{
+            throw new Error()
+        })
         const httpRequest = {
             body: {
                 name: "any_name",
