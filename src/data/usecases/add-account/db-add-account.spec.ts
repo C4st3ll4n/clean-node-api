fda913dfe2e17490c45b81a3a8d3c154303685e8
Pedro Henrique <p13dr0h@gmail.com>
2022-12-12 17:42:37 -0300

    refactor: move sut creation to a helper factory method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,22 +1,41 @@
+import { Encrypter } from "../../protocols/encrypter"
 import { DbAddAccount } from "./db-add-account"
 
-describe("DbAddAccount Usecase", () => {
+interface SutTypes {
+    sut: DbAddAccount,
+    encrypterStub: Encrypter
+}
 
+const makeEncrypterStub = (): Encrypter => {
     class EncrypterStub {
-        async encrypt(value:string): Promise<string>{
+        async encrypt(value: string): Promise<string> {
             return new Promise(resolve => resolve("hashed_password"))
         }
     }
+    return new EncrypterStub()
+}
 
-    test("Should call Encrypter with correct password", ()=> {
-        const encrypterStub = new EncrypterStub()
-        const sut = new DbAddAccount(encrypterStub)
+const makeSut = (): SutTypes => {
+    const encrypterStub = makeEncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+    return {
+        sut,
+        encrypterStub
+    }
+}
+
+describe("DbAddAccount Usecase", () => {
+
+
+
+    test("Should call Encrypter with correct password", () => {
+        const { sut, encrypterStub } = makeSut()
         const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
         const accountData = {
             name: "valid_name",
-            email:"valid_email",
-            password:"valid_password"
+            email: "valid_email",
+            password: "valid_password"
         }
         sut.add(accountData)
         expect(encryptSpy).toBeCalledWith('valid_password')
