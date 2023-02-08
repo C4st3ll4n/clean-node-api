8c66a6d79dcf919c659ad2e6cf58bf20ce76fdef
Pedro Henrique <p13dr0h@gmail.com>
2023-02-08 06:36:08 -0300

    feat: add log controller decorator

---
+++ src/main/decorator/log.spec.ts
@@ -0,0 +1,54 @@
+import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
+import { LogControllerDecorator } from "./log"
+
+interface SutTypes {
+    sut: LogControllerDecorator
+    stub: Controller
+}
+
+const makeStub = ():Controller => {
+    class ControllerStub implements Controller{
+        handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+            const httpResponse: HttpResponse= {
+                statusCode: 200,
+                body:{
+                    name: "Pedro"
+                }
+            }
+            return new Promise(resolve=>resolve(httpResponse))
+        }
+    }
+
+    return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+    const stub = makeStub()
+    const sut = new LogControllerDecorator(stub)
+
+    return {
+        sut,
+        stub
+    }
+}
+
+describe("Log Decorator Tests", () => {
+    test("Should call handle", async ()=>{
+        
+        const {sut, stub} = makeSut()
+        const handleSpy = jest.spyOn(stub, "handle")
+        const httpRequest = {
+            body: {
+                email: "any_mail@mail.com",
+                name:"any_name",
+                password:"any_password",
+                passwordConfirmation:"any_password"
+            }
+        }
+        
+        await sut.handle(httpRequest)
+
+        expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+        
+    })
+})
--- src/main/factory/signup.ts
+++ src/main/factory/signup.ts
@@ -3,8 +3,10 @@ import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { LogControllerDecorator } from "../decorator/log";
+import { Controller } from "../../presentation/protocols";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
     const salt = 12;
     const bcryptAdapter = new BcryptAdapter(salt)
 
@@ -13,6 +15,6 @@ export const makeSignUpController = (): SignUpController => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountRepository)
 
-    return new SignUpController(emailValidatorAdapter, dbAddAccount)
+    return new LogControllerDecorator(new SignUpController(emailValidatorAdapter, dbAddAccount))
 
 }
