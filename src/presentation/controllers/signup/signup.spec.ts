d71334566fe0648b296d318b6fecf5a5450af577
Pedro Henrique <p13dr0h@gmail.com>
2023-03-30 01:55:56 -0300

    feat: ensure SignupController returns bad request when validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -235,5 +235,15 @@ describe("SignUp Controller", () => {
         const httpRequest = makeHttpRequest()
         await sut.handle(httpRequest)
         expect(addSpySpy).toHaveBeenCalledWith(httpRequest.body)
+    })    
+
+    test("Should return 400 if validation returns an error", async () => {
+        const { sut, validationStub } = makeSut()
+
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError("any_field"))
+
+        const httpResponse = await sut.handle(makeHttpRequest())
+
+        expect(httpResponse).toEqual(badRequest(new MissingParamError("any_field")))
     })
 })
