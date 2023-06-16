37d9802bbda7534f6fc10cd36b91f68331373c07
Pedro Henrique <p13dr0h@gmail.com>
2023-06-15 22:48:29 -0300

    feat: add login routes

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
     MONGO_URL: process.env.MONGO_URL|| 'mongodb://localhost:27017/clean-node-api',
-    PORT: process.env.PORT || 5050
+    PORT: process.env.PORT || 5050,
+    SECRET: process.env.JWT_SECRET || "asldh6541!@#^Ç"
 }
