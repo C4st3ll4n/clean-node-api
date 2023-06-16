41d7ff1d516d970310f4eeca2236c0f1e094f22c
Pedro Henrique <p13dr0h@gmail.com>
2023-06-15 21:20:45 -0300

    test: remove debug

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -4,12 +4,8 @@ import app from "../config/app";
 describe("Body Parser Middleware", () => {
     test("Should parse body as json", async()=> {
         app.post("/test_body_parser", (req, res)=> {
-            console.log(req);
             res.send(req.body)
-            console.log(res);
-        })
-
-        
+        })        
 
         await request(app)
         .post("/test_body_parser")
