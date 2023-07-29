ffb44d37802f632a790aa3d4e8cac6e53ab859b7
Pedro Henrique <p13dr0h@gmail.com>
2023-07-29 13:28:22 -0300

    feat: ensure controller create survey return 500 when add survey throws

--- src/presentation/controllers/survey/create-survey/create-survey-controller.ts
+++ src/presentation/controllers/survey/create-survey/create-survey-controller.ts
@@ -1,22 +1,34 @@
-import { badRequest } from "../../../helpers/http/http-helper";
-import { Controller, HttpRequest, HttpResponse, Validation } from "../../../protocols";
+import { badRequest, serverError } from "../../../helpers/http/http-helper";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+} from "../../../protocols";
 import { AddSurvey } from "./create-survey-protocols";
 
 export class CreateSurveyController implements Controller {
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
-    constructor(private readonly validation:Validation, private readonly addSurvey:AddSurvey){}
-
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const error = this.validation.validate(httpRequest.body);
-        if(error){
-            return badRequest(error);
-        }
-
-        const {question, answers} = httpRequest.body;
-        await this.addSurvey.add({
-            question, answers
-        })
-        return null;
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body);
+    if (error) {
+      return badRequest(error);
     }
 
+    const { question, answers } = httpRequest.body;
+    try {
+      await this.addSurvey.add({
+        question,
+        answers,
+      });
+
+      return null;
+    } catch (error) {
+      return serverError(error);
+    }
+  }
 }
