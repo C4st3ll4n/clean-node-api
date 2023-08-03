cecae376d55d027d84c152be58cede02f783ccda
Pedro Henrique <p13dr0h@gmail.com>
2023-08-03 11:40:59 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+    openapi: '3.0.0',
+    info:
+        {
+            title: 'Survey API',
+            description: 'Clean Node Survey API: Create and manage surveys',
+            version: '0.0.1'
+        }
+}
