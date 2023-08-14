c3ccffc6b97234fa01a241da0b70b00d7da50ae3
Pedro Henrique <p13dr0h@gmail.com>
2023-08-13 23:49:25 -0300

    refactor: move dependencies to a protocols file

--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,28 +1,33 @@
-import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
-import { AccessDeniedError } from "../errors";
-import { forbidden, ok, serverError } from "../helpers/http/http-helper";
-import { HttpRequest, HttpResponse } from "../protocols";
-import { Middleware } from "../protocols/middleware";
+import {
+  LoadAccountByToken,
+  AccessDeniedError,
+  forbidden,
+  ok,
+  serverError,
+  HttpRequest,
+  HttpResponse,
+  Middleware,
+} from "./auth-middleware-protocols";
 
-export class AuthMiddleware implements Middleware{
+export class AuthMiddleware implements Middleware {
+  constructor(
+    private readonly loadAccountToken: LoadAccountByToken,
+    private readonly role?: string
+  ) {}
 
-    constructor(private readonly loadAccountToken: LoadAccountByToken, private readonly role?:string){}
-
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const token = httpRequest.headers?.["x-access-token"];
-        try {
-            if(token){
-                const account = await this.loadAccountToken.load(token, this.role)
-                if(account){
-                    return ok({accountId: account.id});
-                }
-            }
-                
-        } catch (error) {
-            return serverError(error)    
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const token = httpRequest.headers?.["x-access-token"];
+    try {
+      if (token) {
+        const account = await this.loadAccountToken.load(token, this.role);
+        if (account) {
+          return ok({ accountId: account.id });
         }
-        
-        return forbidden(new AccessDeniedError())
+      }
+    } catch (error) {
+      return serverError(error);
     }
 
+    return forbidden(new AccessDeniedError());
+  }
 }
