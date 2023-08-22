22f4bf4203e22a31016290052d2bf84e96e87c0a
Pedro Henrique <p13dr0h@gmail.com>
2023-08-22 16:28:58 -0300

    feat: adjust middleware flow

--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,33 +1,39 @@
 import {
-  LoadAccountByToken,
-  AccessDeniedError,
-  forbidden,
-  ok,
-  serverError,
-  HttpRequest,
-  HttpResponse,
-  Middleware,
+    LoadAccountByToken,
+    AccessDeniedError,
+    forbidden,
+    ok,
+    serverError,
+    HttpRequest,
+    HttpResponse,
+    Middleware,
 } from "./auth-middleware-protocols";
 
 export class AuthMiddleware implements Middleware {
-  constructor(
-    private readonly loadAccountToken: LoadAccountByToken,
-    private readonly role?: string
-  ) {}
-
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const token = httpRequest.headers?.["x-access-token"];
-    try {
-      if (token) {
-        const account = await this.loadAccountToken.loadByToken(token, this.role);
-        if (account) {
-          return ok({ accountId: account.id });
-        }
-      }
-    } catch (error) {
-      return serverError(error);
+    constructor(
+        private readonly loadAccountToken: LoadAccountByToken,
+        private readonly role?: string
+    ) {
     }
 
-    return forbidden(new AccessDeniedError());
-  }
+    async handle(request: AuthMiddleware.Request): Promise<HttpResponse> {
+        try {
+            const {token} = request;
+            if (token) {
+                const account = await this.loadAccountToken.loadByToken(token, this.role);
+                if (account) {
+                    return ok({accountId: account.id});
+                }
+            }
+            return forbidden(new AccessDeniedError());
+        } catch (error) {
+            return serverError(error);
+        }
+    }
+}
+
+export namespace AuthMiddleware {
+    export type Request = {
+        token?: string
+    }
 }
