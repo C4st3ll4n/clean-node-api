1b3cbc2a212cfb7d637a4b895d2567de099c0abc
Pedro Henrique <p13dr0h@gmail.com>
2023-08-22 01:53:12 -0300

    test: ensure survey get route return 200 on LoadSurveys successfully

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,119 +1,134 @@
 import request from "supertest";
 import app from "../config/app";
-import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
-import { hash } from "bcrypt";
-import { sign } from "jsonwebtoken";
+import {MongoHelper} from "../../infra/db/mongodb/helpers/mongo-helper";
+import {hash} from "bcrypt";
+import {sign} from "jsonwebtoken";
 import env from "../config/env";
 
 let surveyColletion;
 let accountCollection;
 
 describe("Survey Routes", () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL);
-  });
-
-  afterAll(async () => {
-    await MongoHelper.disconnect();
-  });
-
-  beforeEach(async () => {
-    surveyColletion = await MongoHelper.getCollection("surveys");
-    await surveyColletion.deleteMany({});
-
-    accountCollection = await MongoHelper.getCollection("account");
-    await accountCollection.deleteMany({});
-  });
-
-  describe("POST /surveys", () => {
-    test("Should return 403 on surveys without accessToken", async () => {
-      await request(app)
-        .post("/api/surveys")
-        .send({
-          question: "any_question",
-          answers: [
-            {
-              image: "any_image",
-              answer: "any_answer",
-            },
-          ],
-        })
-        .expect(403);
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL);
     });
 
-    test("Should return 204 on surveys", async () => {
-      const resCreate = await accountCollection.insertOne({
-        name: "Fulano",
-        email: "dulagno@gmail.com",
-        password: hash("123", 12),
-        role: 'admin'
-      });
-
-      const id = resCreate.ops[0]._id;
-      const accessToken = sign({ id }, env.SECRET);
-
-      const resUpdate = await accountCollection.updateOne(
-          {
-            _id: id,
-          },
-          {
-            $set: {
-              accessToken,
-            },
-          }
-      );
-
-      await request(app)
-          .post("/api/surveys")
-          .set("x-access-token", accessToken)
-          .send({
-            question: "any_question",
-            answers: [
-              {
-                image: "any_image",
-                answer: "any_answer",
-              },
-            ],
-          })
-          .expect(204);
+    afterAll(async () => {
+        await MongoHelper.disconnect();
     });
 
-  });
+    beforeEach(async () => {
+        surveyColletion = await MongoHelper.getCollection("surveys");
+        await surveyColletion.deleteMany({});
 
-  describe("GET /surveys", ()=>{
-    test("Should return 403 on LoadSurveys without accessToken", async()=>{
-      await request(app)
-          .get("/api/surveys")
-          .expect(403);
+        accountCollection = await MongoHelper.getCollection("account");
+        await accountCollection.deleteMany({});
     });
 
-    test("Should return 200 on LoadSurveys successfully", async()=>{
+    describe("POST /surveys", () => {
+        test("Should return 403 on surveys without accessToken", async () => {
+            await request(app)
+                .post("/api/surveys")
+                .send({
+                    question: "any_question",
+                    answers: [
+                        {
+                            image: "any_image",
+                            answer: "any_answer",
+                        },
+                    ],
+                })
+                .expect(403);
+        });
 
-      const resCreate = await accountCollection.insertOne({
-        name: "Fulano",
-        email: "dulagno@gmail.com",
-        password: hash("123", 12),
-        role: 'admin'
-      });
+        test("Should return 204 on surveys", async () => {
 
-      const id = resCreate.ops[0]._id;
-      const accessToken = sign({ id }, env.SECRET);
+            surveyColletion.insertMany([
+                {
+                    question: "any_question",
+                    answers: [
+                        {
+                            image: "any_image",
+                            answer: "any_answer",
+                        },
+                    ],
+                    date: new Date()
+                }
+            ]);
 
-      const resUpdate = await accountCollection.updateOne(
-          {
-            _id: id,
-          },
-          {
-            $set: {
-              accessToken,
-            },
-          }
-      );
+            const resCreate = await accountCollection.insertOne({
+                name: "Fulano",
+                email: "dulagno@gmail.com",
+                password: hash("123", 12),
+                role: 'admin'
+            });
 
-      await request(app)
-          .get("/api/surveys")
-          .set("x-access-token", accessToken)
-          .expect(200);
+            const id = resCreate.ops[0]._id;
+            const accessToken = sign({id}, env.SECRET);
+
+            const resUpdate = await accountCollection.updateOne(
+                {
+                    _id: id,
+                },
+                {
+                    $set: {
+                        accessToken,
+                    },
+                }
+            );
+
+            await request(app)
+                .post("/api/surveys")
+                .set("x-access-token", accessToken)
+                .send({
+                    question: "any_question",
+                    answers: [
+                        {
+                            image: "any_image",
+                            answer: "any_answer",
+                        },
+                    ],
+                })
+                .expect(204);
+        });
+
     });
-  })
+
+    describe("GET /surveys", () => {
+        test("Should return 403 on LoadSurveys without accessToken", async () => {
+            await request(app)
+                .get("/api/surveys")
+                .expect(403);
+        });
+
+        test("Should return 200 on LoadSurveys successfully", async () => {
+
+            const resCreate = await accountCollection.insertOne({
+                name: "Fulano",
+                email: "dulagno@gmail.com",
+                password: hash("123", 12),
+                role: 'admin'
+            });
+
+            const id = resCreate.ops[0]._id;
+            const accessToken = sign({id}, env.SECRET);
+
+            const resUpdate = await accountCollection.updateOne(
+                {
+                    _id: id,
+                },
+                {
+                    $set: {
+                        accessToken,
+                    },
+                }
+            );
+
+
+            await request(app)
+                .get("/api/surveys")
+                .set("x-access-token", accessToken)
+                .expect(200);
+        });
+    })
 });
