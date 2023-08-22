754c99b68af5f98aef9804e63da7b4f13f19491e
Pedro Henrique <p13dr0h@gmail.com>
2023-08-22 16:29:35 -0300

    feat: loadByAccount

--- src/data/protocols/db/survey/list-survey-repository.ts
+++ src/data/protocols/db/survey/list-survey-repository.ts
@@ -2,4 +2,5 @@ import { SurveyModel } from "../../../../domain/models/survey";
 
 export interface ListSurveyRepository{
     all():Promise<SurveyModel[]>
+    load(accountId:string):Promise<SurveyModel[]>
 }
--- src/data/usecases/list-survey/db-list-survey.ts
+++ src/data/usecases/list-survey/db-list-survey.ts
@@ -2,14 +2,16 @@ import {ListSurvey} from "../../../domain/usecases/list-survey";
 import {SurveyModel} from "../../../domain/models/survey";
 import {ListSurveyRepository} from "../../protocols/db/survey/list-survey-repository";
 
-export class DBListSurvey implements ListSurvey{
+export class DBListSurvey implements ListSurvey {
 
-
-    constructor(private readonly repository:ListSurveyRepository) {}
-
+    constructor(private readonly repository: ListSurveyRepository) {
+    }
     async getAll(): Promise<SurveyModel[]> {
-
         return await this.repository.all();
+    }
+
+    async load(accountId:string): Promise<SurveyModel[]> {
+        return await this.repository.load(accountId);
     }
 
 }
