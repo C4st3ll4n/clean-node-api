ff4de9cba0ca198b11f85480ec2c249938a21afa
Pedro Henrique <p13dr0h@gmail.com>
2023-08-22 01:12:00 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,17 @@
-import { AddSurveyRepository } from "../../../../data/protocols/db/survey/add-survey-repository";
-import { AddSurveyModel } from "../../../../domain/usecases/add-survey";
-import { MongoHelper } from "../helpers/mongo-helper";
+import {AddSurveyRepository} from "../../../../data/protocols/db/survey/add-survey-repository";
+import {AddSurveyModel} from "../../../../domain/usecases/add-survey";
+import {MongoHelper} from "../helpers/mongo-helper";
+import {ListSurveyRepository} from "../../../../data/protocols/db/survey/list-survey-repository";
+import {SurveyModel} from "../../../../domain/models/survey";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, ListSurveyRepository {
   async add(data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     const result = await surveyCollection.insertOne(data);
+  }
+
+  async all(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    return await surveyCollection.find().toArray();
   }
 }
