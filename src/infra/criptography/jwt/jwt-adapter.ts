262274b19b07c0ee2383763dc83bb31d4832dd6c
Pedro Henrique <p13dr0h@gmail.com>
2023-08-28 16:43:51 -0300

    refactor: adjust adapter to return an object

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,18 +1,18 @@
-import { Decrypter } from "@/data/protocols/criptography/decrypter";
-import { Encrypter } from "@/data/protocols/criptography/encrypter";
+import {Decrypter} from "@/data/protocols/criptography/decrypter";
+import {Encrypter} from "@/data/protocols/criptography/encrypter";
 import jwt from "jsonwebtoken";
+
 export class JWTAdapter implements Encrypter, Decrypter {
 
     constructor(private readonly secret: string) {
     }
-    
+
     async encrypt(value: string): Promise<string> {
-        return jwt.sign({ id: value }, this.secret)
+        return jwt.sign({id: value}, this.secret)
     }
 
-    async decrypt(value: string): Promise<string> {
-        const accessToken = await jwt.verify(value, this.secret);
-        return accessToken.toString()
+    async decrypt(value: string): Promise<object> {
+        return jwt.verify(value, this.secret) as any
     }
 
 }
