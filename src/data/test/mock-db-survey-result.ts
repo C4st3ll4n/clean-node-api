ad4d8b81f9282ddfc7b7fc5bceb413f65c03f1f5
Pedro Henrique <p13dr0h@gmail.com>
2023-09-01 14:57:47 -0300

    refactor: move mocks to a helper file and reuse them

---
+++ src/data/test/mock-db-survey-result.ts
@@ -0,0 +1,15 @@
+import {SurveyResultModel} from "@/domain/models/survey-result";
+import {SaveSurveyResultRepository} from "@/data/protocols/db/survey-result/save-survey-result-repository";
+import {SaveSurveyResultParam} from "@/domain/usecases/survey-result/save-survey-result";
+import {makeFakeSurveyResult} from "@/domain/test/mock-survey-result";
+
+
+export const makeSaveSurveyResultRepositoryStub = (): SaveSurveyResultRepository => {
+    class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+        save(data: SaveSurveyResultParam): Promise<SurveyResultModel> {
+            return Promise.resolve(makeFakeSurveyResult());
+        }
+    }
+
+    return new SaveSurveyResultRepositoryStub();
+}
