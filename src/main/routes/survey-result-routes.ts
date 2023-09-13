bf4ce235207f303b70488263a66f9164dbdfcd83
Pedro Henrique <p13dr0h@gmail.com>
2023-09-13 18:03:43 -0300

    feat: add survey result GET route

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -5,11 +5,19 @@ import {auth} from "../middlewares/auth";
 import {
     makeSaveSurveyResultController
 } from "@/main/factory/controller/survey-result/save-survey-result/save-survey-result-controller-factory";
+import {
+    makeLoadSurveyResultController
+} from "@/main/factory/controller/survey-result/load-survey-result/load-survey-result-controller-factory";
 
 export default (router: Router): void => {
 
     router.put(
         "/surveys/:surveyId/results", auth,
         adaptRoute(makeLogControllerDecorator(makeSaveSurveyResultController()))
+    );
+
+    router.get(
+        "/surveys/:surveyId/results", auth,
+        adaptRoute(makeLogControllerDecorator(makeLoadSurveyResultController()))
     );
 };
