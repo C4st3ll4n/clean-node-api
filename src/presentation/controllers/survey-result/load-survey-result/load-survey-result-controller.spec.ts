f38dd5c24845fb20dafd366b0cf2b592a609a42f
Pedro Henrique <p13dr0h@gmail.com>
2023-09-13 17:11:49 -0300

    test: ensure controller return an survey result on success

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,82 @@
+import {HttpRequest} from "@/presentation/protocols";
+import {LoadSurveyById} from "@/domain/usecases/survey/load-survey-by-id";
+import {badRequest, ok, serverError} from "@/presentation/helpers/http/http-helper";
+import {InvalidParamError} from "@/presentation/errors";
+import mockdate from "mockdate";
+import {makeFakeSurveyResult} from "@/domain/test/mock-survey-result";
+import {
+    LoadSurveyResultController
+} from "@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller";
+import {LoadSurveyResult} from "@/domain/usecases/survey-result/load-survey-result";
+import {SurveyResultModel} from "@/domain/models/survey-result";
+import {throwError} from "@/domain/test";
+import e from "express";
+
+type SUTTypes = {
+    sut: LoadSurveyResultController,
+    loadSurveyResultStub: LoadSurveyResult,
+}
+
+const mockLoadSurveyResult = () : LoadSurveyResult => {
+    return new class LoadSurveyResultSub implements LoadSurveyResult{
+        async load(surveyId:string): Promise<SurveyResultModel> {
+            return Promise.resolve(makeFakeSurveyResult());
+        }
+    }
+};
+const makeSut = (): SUTTypes => {
+    const loadSurveyResultStub = mockLoadSurveyResult();
+    const sut = new LoadSurveyResultController(loadSurveyResultStub)
+    return {
+        sut,
+        loadSurveyResultStub,
+    }
+}
+
+const makeRequest = (): HttpRequest => {
+    return {
+        params: {
+            surveyId: "any_survey_id"
+        },
+        accountId: "any_account_id"
+    };
+};
+describe("Load Survey Result Controller", () => {
+
+    beforeAll(() => {
+        mockdate.set(new Date())
+    })
+
+    describe("Load Survey Result", () => {
+        test("should call LoadSurveyById correctly", async () => {
+            const {sut, loadSurveyResultStub} = makeSut();
+            const spy = jest.spyOn(loadSurveyResultStub, "load")
+            await sut.handle(makeRequest())
+            expect(spy).toHaveBeenCalledWith("any_survey_id")
+        })
+
+        test("should return 404 when LoadSurveyById returns null", async () => {
+            const {sut, loadSurveyResultStub} = makeSut();
+            jest.spyOn(loadSurveyResultStub, "load").mockReturnValueOnce(null)
+            const response = await sut.handle(makeRequest())
+            expect(response.statusCode).toEqual(404)
+        })
+
+        test("should return 500 when LoadSurveyById throws", async () => {
+            const {sut, loadSurveyResultStub} = makeSut();
+            jest.spyOn(loadSurveyResultStub, "load").mockImplementationOnce(throwError);
+            const httpResponse = await sut.handle(makeRequest());
+            expect(httpResponse).toEqual(serverError(new Error()));
+        })
+
+        test("should return an survey result on success", async () => {
+            const {sut} = makeSut();
+            const {body, statusCode} = await sut.handle(makeRequest());
+
+            expect(statusCode).toBe(200)
+            expect(body).toBeTruthy()
+            expect(body.surveyId).toEqual("any_survey_id")
+            expect(body.answers).toBeTruthy()
+        })
+    })
+})
