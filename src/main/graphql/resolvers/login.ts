81655352c8256ef04badca28f64960c1ba52ae05
Pedro Henrique <p13dr0h@gmail.com>
2023-09-20 23:10:31 -0300

    feat: handle graphql signup mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,12 @@
 import {makeLoginController} from "@/main/factory/controller/account/login/login-controller-factory";
 import {adaptResolver} from "@/main/adapter";
+import {makeSignUpController} from "@/main/factory/controller/account/signup/signup-controller-factory";
 
 export default {
     Query: {
         login: async (_: any, args: any) => adaptResolver(makeLoginController(), args)
+    },
+    Mutation:{
+        signUp: async (_:any, args:any) => adaptResolver(makeSignUpController(), args)
     }
 }
